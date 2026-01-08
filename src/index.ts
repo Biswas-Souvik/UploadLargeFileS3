589d52cd4a04691dfbae55ff4712452f7fd808b5
Souvik Biswas <biswassouvik284@gmail.com>
2026-01-08 12:57:56 +0530

    automate single and multipart upload

--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,22 @@ interface IntiateResponseData {
 }
 
 interface UrlResponseData {
-  partNumber: string;
+  partNumber: number;
   url: string;
 }
 
 const API_BASE_URL = process.env.API_BASE_URL!;
-const LOCAL_FILE_PATH = '.uploads/PokeAPI Walkthrough.mp4';
-const PART_SIZE = 5 * 1024 * 1024; // 5 MB in bytes
+const LOCAL_FILE_PATH_M = '.uploads/large_file_777.msi';
+const LOCAL_FILE_PATH_S = '.uploads/large_file_755.msi';
+const PART_SIZE = 20 * 1024 * 1024;
 
-async function uploadFile() {
+async function multiPartUpload(filePath: string) {
   try {
     const startTime = Date.now();
-    const fileName = path.basename(LOCAL_FILE_PATH);
-    const mimeType = mime.lookup(LOCAL_FILE_PATH);
+    const fileName = path.basename(filePath);
+    const mimeType = mime.lookup(filePath);
 
-    const fileSize = fs.statSync(LOCAL_FILE_PATH).size;
+    const fileSize = fs.statSync(filePath).size;
 
     const initiateResp = await fetch(`${API_BASE_URL}/multipart/initiate`, {
       method: 'POST',
@@ -38,7 +39,7 @@ async function uploadFile() {
       throw new Error('Initiate upload failed');
     }
     const initiateRespData = (await initiateResp.json()) as IntiateResponseData;
-    console.log('Initiate Upload Resp: ', initiateRespData);
+    // console.log('Initiate Upload Resp: ', initiateRespData);
 
     const { uploadId, key } = initiateRespData;
     if (!uploadId || !key) {
@@ -56,41 +57,53 @@ async function uploadFile() {
       throw new Error('Failed to get presigned URLs');
     }
     const urlRespData = (await urlResp.json()) as UrlResponseData[];
-    console.log('Presigned URLs: ', urlRespData);
+    // console.log('Presigned URLs: ', urlRespData);
 
     const totalParts = urlRespData.length;
 
-    const fileStream = fs.createReadStream(LOCAL_FILE_PATH, {
+    const fileStream = fs.createReadStream(filePath, {
       highWaterMark: PART_SIZE,
     });
 
     let i = 0;
-    const uploadedParts = [];
+    const uploadedParts: { partNumber: number; ETag: string }[] = [];
+
+    const CONCURRENCY = 50;
+    let inFlight = [];
 
     for await (const chunk of fileStream) {
       const { partNumber, url } = urlRespData[i];
 
-      const res = await fetch(url, {
+      const uploadPromise = fetch(url, {
         method: 'PUT',
         body: chunk,
+      }).then(async (res) => {
+        if (!res.ok) {
+          throw new Error(`Upload failed at part ${partNumber}`);
+        }
+
+        const etag = res.headers.get('ETag');
+        if (!etag) throw new Error('ETag missing');
+
+        uploadedParts.push({ partNumber, ETag: etag });
+        console.log(`Uploaded part ${partNumber}/${totalParts}`);
       });
-      if (!res.ok) {
-        throw new Error(`Upload failed at part ${i}: ${await res.text()}`);
+
+      inFlight.push(uploadPromise);
+
+      if (inFlight.length === CONCURRENCY) {
+        await Promise.all(inFlight);
+        inFlight = [];
       }
 
-      const etag = res.headers.get('ETag');
-      if (!etag) throw new Error('ETag missing');
-
-      console.log(res.headers);
-
-      uploadedParts.push({
-        partNumber,
-        ETag: etag,
-      });
-      console.log(`Uploaded part ${i + 1}/${totalParts}`);
       i++;
     }
 
+    // wait for remaining uploads
+    if (inFlight.length) {
+      await Promise.all(inFlight);
+    }
+    uploadedParts.sort((a, b) => a.partNumber - b.partNumber);
     const completeRes = await fetch(`${API_BASE_URL}/multipart/complete`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -104,10 +117,87 @@ async function uploadFile() {
 
     console.log('Multipart Upload Completed');
     console.log('Time Taken: ', (Date.now() - startTime) / 1000, 'seconds');
+    console.log(
+      'Uploaded File Size: ',
+      (fileSize / (1024 * 1024)).toFixed(2),
+      'MB'
+    );
     console.log('File Location: ', completeRespData.Location);
   } catch (err: unknown) {
     console.error('Error during upload: ', (err as Error).message);
   }
 }
 
-uploadFile();
+async function singleUpload(filePath: string) {
+  try {
+    const fileName = path.basename(filePath);
+    const fileSize = fs.statSync(filePath).size;
+    const uploadPath = `presigned-url?fileName=${fileName}`;
+    console.log('API URL Path:', uploadPath);
+
+    const apiUrl = `${API_BASE_URL}/${uploadPath}`;
+
+    const presignRes = await fetch(apiUrl);
+
+    if (!presignRes.ok) {
+      throw new Error(`Failed to get presigned URL: ${presignRes.status}`);
+    }
+
+    const data = (await presignRes.json()) as {
+      url: string;
+      fields: Record<string, string>;
+    };
+    const { url, fields } = data;
+
+    console.log('S3 URL:', url);
+    console.log('Fields returned:', Object.keys(fields));
+
+    const form = new FormData();
+
+    for (const [key, value] of Object.entries(fields)) {
+      form.append(key, value as string);
+    }
+
+    console.log('Attaching file:', filePath);
+    const buffer = fs.readFileSync(filePath);
+
+    form.append('file', new File([buffer], fileName), fileName);
+
+    console.log('Uploading file to S3');
+    const startTime = Date.now();
+    const uploadRes = await fetch(url, {
+      method: 'POST',
+      body: form as any,
+    });
+
+    console.log('Upload status:', uploadRes.status);
+    if (!uploadRes.ok) {
+      console.error(await uploadRes.text());
+    }
+
+    if (uploadRes.status === 204) {
+      console.log('Single Upload Completed');
+      console.log('Time Taken: ', (Date.now() - startTime) / 1000, 'seconds');
+      console.log(
+        'Uploaded File Size: ',
+        (fileSize / (1024 * 1024)).toFixed(2),
+        'MB'
+      );
+      console.log('File Location: ', uploadRes.headers.get('location'));
+    } else {
+      console.error('Upload failed: ', await uploadRes.text());
+    }
+  } catch (err: unknown) {
+    console.error('Error during upload: ', (err as Error).message);
+  }
+}
+
+async function automation() {
+  await multiPartUpload(LOCAL_FILE_PATH_M);
+  console.log(
+    '\n\n-----------------------------------------------------------------------------------------------\n\n'
+  );
+  await singleUpload(LOCAL_FILE_PATH_S);
+}
+
+automation();
